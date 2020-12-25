9610da2ab9ffd3d6b3f5dd30f539e5679ae1302f
Uday Sharma <sharmauday1999@gmail.com>
2020-12-24 16:02:13 -0800

    Setting up React app

---
+++ src/config.js
@@ -0,0 +1,14 @@
+const config = {
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://1atjtpm80j.execute-api.us-east-1.amazonaws.com/dev",
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_DKan1agM4",
+      APP_CLIENT_ID: "65oomsm3r09et5qfhmebc3cj30",
+      IDENTITY_POOL_ID: "us-east-1:c88ec78d-4789-42c6-aeaf-289cada5ec7a",
+    },
+  };
+  
+  export default config;
---
+++ src/containers/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className="lander">
+        <h1>Shoppies Nominations</h1>
+        <p className="text-muted">Log in to nominate movies</p>
+      </div>
+    </div>
+  );
+}
