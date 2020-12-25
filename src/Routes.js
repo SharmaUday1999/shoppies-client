b233bcadd81bc262b5112830e67f7144e20676d5
Uday Sharma <sharmauday1999@gmail.com>
2020-12-24 17:14:11 -0800

    integrate auth

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,11 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
+import Login from "./containers/Login";
+import Signup from "./containers/Signup";
+import Nominate from "./containers/Nominate";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default function Routes() {
   return (
@@ -9,10 +14,19 @@ export default function Routes() {
       <Route exact path="/">
         <Home />
       </Route>
+      <UnauthenticatedRoute  exact path="/login">
+        <Login />
+      </UnauthenticatedRoute >
+      <UnauthenticatedRoute  exact path="/signup">
+        <Signup />
+      </UnauthenticatedRoute >
+      <AuthenticatedRoute  exact path="/nominate">
+        <Nominate />
+      </AuthenticatedRoute >
       {/* Finally, catch all unmatched routes */}
-    <Route>
-        <NotFound/>
-    </Route>
+      <Route>
+        <NotFound />
+      </Route>
     </Switch>
   );
 }
---
+++ src/containers/Nominate.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./Nominate.css";
+
+export default function Nominate() {
+  return (
+    <div className="Nominate">
+      <div className="lander">
+        <h1>AHHHHHHHHHH</h1>
+      </div>
+    </div>
+  );
+}
